41d475d8f68dcff1666606607b72b38241227a04
Nytyr <nytyr@me.com>
2013-09-19 22:12:13 +0200

    Creating custom and temporal progress dialog.

---
+++ src/com/memetro/android/common/MemetroProgress.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013 Nytyr [me at nytyr dot me]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.memetro.android.common;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.Window;
+
+import com.memetro.android.R;
+
+public class MemetroProgress extends Dialog {
+    Context mContext;
+    View v = null;
+
+    public MemetroProgress(Context context) {
+        super(context);
+        mContext = context;
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.progress_dialog);
+        v = getWindow().getDecorView();
+        v.setBackgroundResource(android.R.color.transparent);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+    }
+
+    @Override
+    public void setTitle(int titleId) {
+        super.setTitle(titleId);
+    }
+
+}
