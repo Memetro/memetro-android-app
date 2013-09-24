70b6070f4ff88e871fb5e27d4946853d6efe9ffd
Nytyr <nytyr@me.com>
2013-09-24 22:11:39 +0200

    Removing unused imports.

--- src/com/memetro/android/MainActivity.java
+++ src/com/memetro/android/MainActivity.java
@@ -16,7 +16,6 @@
 
 package com.memetro.android;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -28,7 +27,6 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.activeandroid.ActiveAndroid;
 import com.memetro.android.common.AppContext;
@@ -47,7 +45,6 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class MainActivity extends Activity {
