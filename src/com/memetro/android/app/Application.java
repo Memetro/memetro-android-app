c74ca1e2bea487c004b271d6da0537162bf8c717
Nytyr <nytyr@me.com>
2014-01-25 12:31:34 +0100

    Simple migration manager

---
+++ src/com/memetro/android/app/Application.java
@@ -0,0 +1,28 @@
+package com.memetro.android.app;
+
+import android.content.Context;
+
+import com.activeandroid.ActiveAndroid;
+import com.activeandroid.Configuration;
+
+public class Application extends android.app.Application {
+
+    private final int schemaVersion = 1;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Context context = getApplicationContext();
+        new MigrationManager(context).initialize(schemaVersion);
+        ActiveAndroid.initialize(
+                new Configuration.Builder(context).setDatabaseName(MigrationManager.DB_NAME).create()
+        );
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        ActiveAndroid.dispose();
+    }
+
+}
---
+++ src/com/memetro/android/app/MigrationManager.java
@@ -0,0 +1,52 @@
+package com.memetro.android.app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MigrationManager {
+
+    private Context context;
+    public static final String DB_NAME = "Memetro";
+    private static final String SHARED_NAME = "cache_shared_prefs";
+    private static final String SCHEMA_VERSION = "cache_shared_prefs_schema";
+
+    public MigrationManager(Context context) {
+        this.context = context;
+    }
+
+    public void initialize(int version) {
+        int oldVersion = getSchemaVersion();
+        if (oldVersion == 0) {
+            setSchemaVersion(version);
+            return;
+        }
+
+        if (oldVersion<version) {
+            deleteSQLite();
+            setSchemaVersion(version);
+        }
+
+    }
+
+    public int getSchemaVersion(){
+        final SharedPreferences prefs = getCachePreferences();
+        return prefs.getInt(SCHEMA_VERSION, 0);
+    }
+
+    public void setSchemaVersion(int verion){
+        final SharedPreferences prefs = getCachePreferences();
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(SCHEMA_VERSION, verion);
+        editor.commit();
+    }
+
+    private SharedPreferences getCachePreferences() {
+        return context.getSharedPreferences(SHARED_NAME,
+                Context.MODE_PRIVATE);
+    }
+
+    private void deleteSQLite() {
+        context.deleteDatabase(DB_NAME);
+    }
+
+}
